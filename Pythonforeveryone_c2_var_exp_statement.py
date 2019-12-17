8fc50684a12a7d16ca2321e54ac45d31cd3caddc
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-17 11:00:44 -0500

    commit

--- Pythonforeveryone_c2_var_exp_statement.py
+++ Pythonforeveryone_c2_var_exp_statement.py
@@ -11,7 +11,25 @@ print(1,000,000)
 
 #variables
 
-message = "and now for something different"
+
 
 n = 17
 print(n)
+
+message = "and now for something different"
+type(message)
+
+
+#statements
+print(1)
+
+y1 = 4
+print(y1)
+
+a = 2 + 3
+print (a)
+
+#operators and operands
+
+minute = 59
+print(float(minute/60))
