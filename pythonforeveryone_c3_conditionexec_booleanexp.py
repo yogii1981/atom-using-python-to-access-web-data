dcd5ff5b1d0f4812eb7cac3f27798b2097f4fbc5
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-18 15:27:26 -0500

    commit

--- pythonforeveryone_c3_conditionexec_booleanexp.py
+++ pythonforeveryone_c3_conditionexec_booleanexp.py
@@ -154,3 +154,49 @@ try:
                         print("Grade is F")
 except:
     print('bad score')
+
+
+score = 0.0                                # Initialize variables
+grade = ""
+
+input1 = input('Enter score: ')
+try:
+    score = float(input1)                  # Only allows input floats
+except ValueError:
+    print('Bad score')
+    quit()
+
+if 0.0 <= score <= 1.0:                    # Scores must be between 0.0 and 1.0
+    if score >= 0.9:
+        grade = 'A'
+    elif score >= 0.8:
+        grade = 'B'
+    elif score >= 0.7:
+        grade = 'C'
+    elif score >= 0.6:
+        grade = 'D'
+    else:
+        grade = 'F'
+else:
+    grade = 'Bad score'
+
+print(grade)
+
+score = float(input('Enter score:') )
+try:
+    if score >= 0.9 and score < 1.0:
+        print("Grade is A")
+    else:
+        if score >= 0.8 and score < 0.9:
+            print("Grade is B")
+        else:
+            if score >= 0.7 and score < 0.8:
+                print("Grade is C")
+            else:
+                if score >=0.6 and score < 0.7:
+                    print("Grade is D")
+                else:
+                    if score < 0.6 and score <=0.0:
+                        print("Grade is F")
+except:
+    print('bad score')
