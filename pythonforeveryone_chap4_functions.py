f797ef0287333dbf9ab15b33df3f07cc38327732
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-19 12:09:21 -0500

    commit

--- pythonforeveryone_chap4_functions.py
+++ pythonforeveryone_chap4_functions.py
@@ -21,8 +21,27 @@ print(str(32))  #convert its arguement to a string
 
 #random numbers
 #write a program to generate random numbers between 0.0 and 1.0
-import random
+from random import random
 
 for i in random(10):
     x = random.random()
     print(x)
+
+# using unifrom method
+from random import random
+x = random.uniform(0.0, 1.0) in range(10)
+print(x)
+
+# randomly selecting grretings
+
+from random import random
+
+greetings = ["Namaste", "Ola", "Hello", "Wie gehts"]
+value = random.choice(greetings)
+print(value + ' , Monica!')
+
+#to choose any element from a squence at randon , 
+# you can use choice
+import random
+t = [1,2,3]
+random.choice(t)
