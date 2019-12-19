9f58b9f13fd65c1458791bf997aceedae3175bc6
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-18 23:14:42 -0500

    commit

--- pythonforeveryone_chap4_functions.py
+++ pythonforeveryone_chap4_functions.py
@@ -1,5 +1,6 @@
 type(32)
 
 max("hello world")
-print(max("hellow world"))
-print(min("hello world"))
+print(max("hellow world")) # find the largest character
+print(min("hello world")) # find the minimum character
+print(len("Hello World"))  #length of hello World
