e35564419ccd3f51270e581b258b61e72e8c323e
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-26 16:53:36 -0500

    commit

--- pythonforeveryone_chap5_Iterations.py
+++ pythonforeveryone_chap5_Iterations.py
@@ -5,5 +5,10 @@ x = x + 1 # gets current value of x , add 1 and update value of x
 # print(x) will give NameError : Name
 # 'x' is not defined\
 x = 0
-x = x + 1
+x = x + 1 # updating a value by incrementing the value by 1
+print(x)
+
+
+x = 5
+x = x - 1  # updating a value by decrementing the value by 1
 print(x)
