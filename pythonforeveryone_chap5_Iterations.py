b07e325aff88442a8fb4c67b281ded5ec9e9d2e6
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-27 01:43:33 -0500

    Commit

--- pythonforeveryone_chap5_Iterations.py
+++ pythonforeveryone_chap5_Iterations.py
@@ -12,3 +12,26 @@ print(x)
 x = 5
 x = x - 1  # updating a value by decrementing the value by 1
 print(x)
+
+# The While statement
+
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+    print('Blastoff')
+# example of finite loop and n = 5 is an iteration variable
+    """
+        while n is greater than zero, display the value n ,
+        and reduce the value of n by 1 and when you get to 0 then exit abd display
+        the word "blastoff"
+    """
+
+# example of infinite loop
+
+while True:
+    line = input(' > ')
+    if line == 'done':
+        break
+        print(line)
+    print('Done!')
