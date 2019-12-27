ec85b24bacddea0d032d9f65dfa2aacce222eb28
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-27 03:35:26 -0500

    commit

--- pythonforeveryone_chap5_Iterations.py
+++ pythonforeveryone_chap5_Iterations.py
@@ -101,3 +101,72 @@ def min(values):
         if smallest is None or value < smallest:
             smallest = value
         return smallest
+
+"""
+Execrise 5.1 write a program which repeatedly read numbers
+until the user enters "done". Once "done" is entered,
+print out the total, count, and average numbers. if the
+user enters anything other than number, detect their mistake
+using try and except and print an error message and skip to
+the next number.
+
+Enter a number: 4
+Enter a number: 5
+Enter a number: bad data
+Invalid input
+Enter a number: 7
+Enter a number: done
+"""
+
+Number = input(Enter a number: )
+count = 0
+total = 0
+try:
+    score = float(Number)
+except ValueError:
+    print('Invalid input')
+    quit()
+
+for a number in Number:
+    count = count + 1
+    total = total + 1
+    average = number / count
+    print("Total:", total , "Count:", count, "Average: ", average)
+
+
+
+def check_for_float(input1):
+    """
+    Checks if the type of "input1" is a float and returns the value if so.
+    Input:    input1 -- variable to check
+    Output: val -- value of float
+    """
+    try:
+        val = float(input1)                   # Only allows input floats
+        return val
+    except ValueError:
+        print('Error, please enter numeric input')
+        quit()
+
+# Check module name since check_for_float is being imported in the next
+# exercise. See also, https://www.youtube.com/watch?v=sugvnHA7ElY (video)
+# or https://www.tutorialspoint.com/python/python_modules.htm (text)
+if __name__ == "__main__":
+    count = 0                                 # Initializes values
+    total = 0.0
+    average = 0.0
+
+    while True:                               # Stays in loop until break
+        input_number = input('Enter a number: ')
+        if input_number == 'done':
+            break                             # Exits the while loop
+
+        number = check_for_float(input_number)
+
+        count += 1                            # Counter
+        total = total + number                # Running total
+
+    # Ensures count is not 0 which would cause division error
+    if count:
+        average = total / count               # Computes the average
+    print(total, count, average)
