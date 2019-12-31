8514f649d9cc09ba7b2ac11aef1242970a8d4985
yogii1981 <yogeshsharma1981@gmail.com>
2019-12-31 18:55:16 -0500

    commit

--- pythonforeveryone_chapter6_Strings.py
+++ pythonforeveryone_chapter6_Strings.py
@@ -28,3 +28,20 @@ while index < len(fruit):
     letter = fruit[index]
     print(letter)
     index = index + 1
+
+"""
+this loop transverses the string and displays each letter
+on a line by itself. The loop is condition is
+index \< len (fruit), so when index is equal to length of the
+string , the condition is false
+"""
+
+
+"""
+Execrise 6.1 : write a while loop that starts at the last character
+in the string and works its ways backward to first character in th string, printing
+each letter on a separate line , except backwards.
+
+fruit = 'banana'
+for char in fruit:
+    print(char)
