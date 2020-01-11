b9c43914b753192ddeed4c2000b80ee1b29271d0
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-11 14:48:15 -0500

    Commit

--- pythonforeveryone_chapter7_Files.py
+++ pythonforeveryone_chapter7_Files.py
@@ -7,3 +7,17 @@ fhand
 
 stuff = "Hello\nword!"
 print(stuff)
+
+fhand = open('top50.csv')
+count = 0
+for line in fhand:
+    count = count + 1
+print('Line Count:', count)
+
+fhand = open('top50.csv')
+inp = fhand.read()
+print(len(imp))
+
+raft = open('sample1.txt')
+inp1 = raft.read()
+print(len(inp1))
