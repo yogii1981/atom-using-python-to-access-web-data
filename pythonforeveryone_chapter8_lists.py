0b95358901b3466cbdbc57234539b01b0f4acc31
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-11 15:35:13 -0500

    commit

--- pythonforeveryone_chapter8_lists.py
+++ pythonforeveryone_chapter8_lists.py
@@ -1,7 +1,10 @@
 """Lists"""
 
 a1 = ['spam', 2.0, 5,[10,20]]
-len(a1)
-print(a1[1:3])
-print(a1[-1])
-print(a1[-1][-1])
+len(a1) # show the length of the list
+print(a1[1:3]) #show the elements at position 1, 2
+print(a1[-1])   #show the last element of the list
+print(a1[-1][-1]) #show the last element present within the last element of the list
+
+cheeses = ['Cheddar','Edam','Gouda']
+print(cheeses[-1])
