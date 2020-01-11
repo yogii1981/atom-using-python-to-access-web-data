d884689810f7b229d943d966220383d6211bf922
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-11 15:19:05 -0500

    commit

--- pythonforeveryone_chapter7_Files.py
+++ pythonforeveryone_chapter7_Files.py
@@ -21,3 +21,29 @@ print(len(imp))
 raft = open('sample1.txt')
 inp1 = raft.read()
 print(len(inp1))
+print(inp1[:50
+
+""" write a file in small chunk to fit a memory"""
+
+raft = open('sample1.txt')
+count = 0
+for line in raft:
+    if line.startswith('The'):
+        print(line)
+
+raft = open('sample1.txt')
+for line in raft:
+    line = line.rstrip()
+    # skip 'uninteresting lines'
+    if not line.startswith('A'):
+        continue
+        #Process our 'interesting' lines
+        print(line)
+
+fname = input('Enter the file name:')
+raft = open(fname)
+count = 0
+for line in raft:
+    if line.startswith('t'):
+        count = count + 1
+    print('There was', count, 'subject lines in', fname)
