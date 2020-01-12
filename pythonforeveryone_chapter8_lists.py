45fb43f5dfd4c08b3f64efd1de9b35a912132796
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-11 23:52:12 -0500

    commit

--- pythonforeveryone_chapter8_lists.py
+++ pythonforeveryone_chapter8_lists.py
@@ -96,3 +96,17 @@ print(max(nums))
 print(min(nums))
 print(sum(nums))
 print(sum(nums)/len(nums))
+
+"""Write a program to compute
+an average without a list:"""
+
+total = 0
+count = 0
+while(True):
+    inp = input('Enter a number:')
+    if inp == 'done': break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+    average = total / count
+print('Average:', average)
