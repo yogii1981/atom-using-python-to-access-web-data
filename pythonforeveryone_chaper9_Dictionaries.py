deca5f571aba1a948cc406b7207103fa5450584d
yogii1981 <yogeshsharma1981@gmail.com>
2020-01-16 16:34:41 -0500

    commit

--- pythonforeveryone_chaper9_Dictionaries.py
+++ pythonforeveryone_chaper9_Dictionaries.py
@@ -3,3 +3,10 @@ unlike Lists where it is only integer"""
 
 eng2sp = dict()
 print(eng2sp)
+
+# to add item to dictionalry use the square bracket
+eng2sp['One'] = "uno"
+print(eng2sp)
+
+eng2sp['two'] = "rino"
+print(eng2sp)
